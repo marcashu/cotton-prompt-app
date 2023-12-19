6ba4082602916dbd15a7f8ed50b9c8465f8aba29
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2023-12-19 10:56:32 +0800

    setup shadcdn

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,69 @@
+import { cn } from "@/lib/utils"
+
+const TypographyH1 = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: React.ReactNode
+}) => {
+  return (
+    <h1
+      className={`scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ${className}`}
+    >
+      {children}
+    </h1>
+  )
+}
+
+const TypographyH2 = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: React.ReactNode
+}) => {
+  return (
+    <h2
+      className={cn(
+        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+        className
+      )}
+    >
+      {children}
+    </h2>
+  )
+}
+
+const TypographyH3 = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: React.ReactNode
+}) => {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-2xl font-semibold tracking-tight",
+        className
+      )}
+    >
+      {children}
+    </h3>
+  )
+}
+
+const TypographyMuted = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: React.ReactNode
+}) => {
+  return (
+    <p className={cn("text-sm text-muted-foreground", className)}>{children}</p>
+  )
+}
+
+export { TypographyH1, TypographyH2, TypographyH3, TypographyMuted }
