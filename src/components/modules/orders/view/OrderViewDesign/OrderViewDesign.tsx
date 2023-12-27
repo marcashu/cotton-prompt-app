7b58be4da25e1623c95bb81bfbfe5de7a4dd247e
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2023-12-27 11:55:55 +0800

    updated design submit to be artist exclusive

--- src/components/modules/orders/view/OrderViewDesign/OrderViewDesign.tsx
+++ src/components/modules/orders/view/OrderViewDesign/OrderViewDesign.tsx
@@ -4,15 +4,31 @@ import { Card } from "@/components/ui/card"
 import { TypographyH3, TypographyMuted } from "@/components/ui/typography"
 import OrderViewDesignUpload from "./OrderViewDesignUpload"
 import GetOrderModel from "@/types/getOrderModel"
+import useSession from "@/hooks/useSession"
+import OrderViewDesignPreview from "./OrderViewDesignPreview"
 
 export default function OrderViewDesign({ order }: { order: GetOrderModel }) {
+  const { session } = useSession()
+
+  if (!session) return <></>
+
+  const isArtist = session.userRole === "artist"
+
   return (
     <Card className="py-6 container shadow flex flex-col gap-4">
       <TypographyH3>
-        Designs
-        <TypographyMuted>Upload your design</TypographyMuted>
+        Design
+        <TypographyMuted>
+          {isArtist ? "Upload your design" : "View the current design"}
+        </TypographyMuted>
       </TypographyH3>
-      <OrderViewDesignUpload order={order} />
+      {isArtist ? (
+        <OrderViewDesignUpload order={order} />
+      ) : (
+        <OrderViewDesignPreview
+          url={order.designs.at(order.designs.length - 1)?.url}
+        />
+      )}
     </Card>
   )
 }
---
+++ src/components/modules/orders/view/OrderViewDesign/OrderViewDesignPreview.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image"
+import { Image as ImageIcon } from "lucide-react"
+
+export default function OrderViewDesignPreview({ url }: { url?: string }) {
+  return (
+    <div className="flex flex-col gap-4 self-center max-w-lg w-full">
+      <div className="flex justify-center items-center rounded-lg border border-dashed border-gray-900/25 relative aspect-video w-full">
+        {!!url ? (
+          <Image
+            src={url}
+            alt="design preview"
+            fill
+            className="object-contain"
+          />
+        ) : (
+          <div className="text-center px-6 py-10">
+            <ImageIcon
+              className="mx-auto h-12 w-12 text-gray-300"
+              aria-hidden="true"
+            />
+            <div className="mt-4 flex text-sm leading-6 text-gray-600">
+              <p className="pl-1">No design uploaded yet</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
