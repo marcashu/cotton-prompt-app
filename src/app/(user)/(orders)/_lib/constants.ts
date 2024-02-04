197cee768655dce3c4a67e10ff750ccc73a59e4c
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2024-02-05 02:21:29 +0800

    added completed artist status

--- src/app/(user)/(orders)/_lib/constants.ts
+++ src/app/(user)/(orders)/_lib/constants.ts
@@ -8,6 +8,7 @@ export enum ArtistStatus {
   Claimed = "Claimed",
   DesignSubmitted = "Design Submitted",
   ForReupload = "For Reupload",
+  Completed = "Completed",
 }
 
 export enum CheckerStatus {
--- src/app/(user)/(orders)/your-orders-as-artist/completed/page.tsx
+++ src/app/(user)/(orders)/your-orders-as-artist/completed/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import useSession from "@/hooks/useSession"
-import { CheckerStatus } from "../../_lib/constants"
+import { ArtistStatus } from "../../_lib/constants"
 import YourOrdersPage from "../../_components/YourOrdersPage"
 
 export default function YourOrdersAsArtistCompletedPage() {
   const { session } = useSession()
-  const url = `/api/orders?artistId=${session?.userId}&checkerStatus=${CheckerStatus.Approved}`
+  const url = `/api/orders?artistId=${session?.userId}&artistStatus=${ArtistStatus.Completed}`
 
   return <YourOrdersPage title="Completed" url={url} />
 }
