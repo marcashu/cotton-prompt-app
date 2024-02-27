1789e79cb0c87358469d978337dc8ecc2295cfe5
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2024-02-28 01:08:26 +0800

    design brackets - implemented defining values

--- src/app/(user)/settings/design-brackets/_components/DesignBracketCreate.tsx
+++ src/app/(user)/settings/design-brackets/_components/DesignBracketCreate.tsx
@@ -1,53 +1,84 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { ChangeEvent, FormEvent, useState } from "react"
+import Input from "@/components/custom/Input"
+import { useState } from "react"
 import { createDesignBracket } from "../_lib/designBracketActions"
 import useSession from "@/hooks/useSession"
 import { useToast } from "@/components/ui/use-toast"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form } from "@/components/ui/form"
+import { DollarSign, Minus } from "lucide-react"
+
+const formSchema = z.object({
+  name: z.string().min(1).max(50),
+  value: z.coerce.number().gt(0),
+})
+
+type DesignBracketCreateFormType = z.infer<typeof formSchema>
 
 export default function DesignBracketCreate() {
-  const [name, setName] = useState("")
   const [loading, setLoading] = useState(false)
   const { session } = useSession()
   const { toast } = useToast()
+  const form = useForm<DesignBracketCreateFormType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      value: 0,
+    },
+  })
 
   if (!session) return <></>
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  const handleSubmit = (data: DesignBracketCreateFormType) => {
     setLoading(true)
-    createDesignBracket(name, 0, session.userId)
+    createDesignBracket(data.name, data.value, session.userId)
       .then(() => {
         toast({
           title: "Design bracket has been created successfully",
           description: new Date().toLocaleString(),
         })
-        setName("")
+        form.reset()
       })
       .finally(() => setLoading(false))
   }
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
-    setName(event.target.value)
-
   return (
-    <form className="flex w-full gap-2" onSubmit={handleSubmit}>
-      <Input
-        type="text"
-        className="w-[350px]"
-        value={name}
-        onChange={handleChange}
-      />
-      <Button
-        type="submit"
-        variant="outline"
-        disabled={!name}
-        loading={loading}
+    <Form {...form}>
+      <form
+        className="flex w-full gap-2 items-center"
+        onSubmit={form.handleSubmit(handleSubmit)}
       >
-        Add
-      </Button>
-    </form>
+        <Input
+          inputClassName="w-[230px]"
+          control={form.control}
+          name="name"
+          placeholder="Name"
+          hideFormMessage
+        />
+        <Minus className="h-4 w-4" />
+        <div className="relative">
+          <DollarSign className="absolute left-2 top-3 h-4 w-4" />
+          <Input
+            inputClassName="w-[100px] pl-8"
+            control={form.control}
+            name="value"
+            placeholder="Value"
+            hideFormMessage
+          />
+        </div>
+        <Button
+          type="submit"
+          variant="outline"
+          disabled={!form.formState.isValid}
+          loading={loading}
+        >
+          Add
+        </Button>
+      </form>
+    </Form>
   )
 }
