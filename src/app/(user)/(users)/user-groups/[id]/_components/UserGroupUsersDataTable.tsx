16bd3b4490c0a52af827f181b03a8d36281c352b
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2024-04-23 17:46:13 +0800

    Implemented removing of user from usergroups

--- src/app/(user)/(users)/user-groups/[id]/_components/UserGroupUsersDataTable.tsx
+++ src/app/(user)/(users)/user-groups/[id]/_components/UserGroupUsersDataTable.tsx
@@ -7,60 +7,98 @@ import Link from "next/link"
 import Role, { isAdmin, trimAdmin } from "@/enums/role"
 import { TypographySmall } from "@/components/ui/typography"
 import { Badge } from "@/components/ui/badge"
-
-const columnDef: ColumnDef<GetUsersModel>[] = [
-  {
-    accessorKey: "name",
-    header: "Name",
-    cell: ({ row }) => {
-      const user = row.original
-      return (
-        // <Link href={`/users/${user.id}`} className="font-medium">
-        //   {user.name}
-        // </Link>
-        <TypographySmall>{user.name}</TypographySmall>
-      )
-    },
-  },
-  {
-    accessorKey: "email",
-    header: "Email",
-  },
-  {
-    header: "Roles",
-    cell: ({ row }) => {
-      const user = row.original
-
-      if (!user.roles.length) return "-"
-
-      const roles = trimAdmin(user.roles)
-
-      return (
-        <div className="flex gap-2">
-          {roles.map((ur) => (
-            <Badge
-              variant={
-                isAdmin(ur)
-                  ? "default"
-                  : ur === Role.Checker
-                  ? "secondary"
-                  : "outline"
-              }
-              key={ur}
-            >
-              {ur}
-            </Badge>
-          ))}
-        </div>
-      )
-    },
-  },
-]
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import RemoveUserDialog from "./RemoveUserDialog"
 
 export default function UserGroupUsersDataTable({
+  id,
   data,
 }: {
+  id: number
   data: GetUsersModel[]
 }) {
-  return <DataTable columns={columnDef} data={data} />
+  const [open, setOpen] = useState(false)
+  const [userId, setUserId] = useState("")
+
+  const columnDef: ColumnDef<GetUsersModel>[] = [
+    {
+      accessorKey: "name",
+      header: "Name",
+      cell: ({ row }) => {
+        const user = row.original
+        return (
+          // <Link href={`/users/${user.id}`} className="font-medium">
+          //   {user.name}
+          // </Link>
+          <TypographySmall>{user.name}</TypographySmall>
+        )
+      },
+    },
+    {
+      accessorKey: "email",
+      header: "Email",
+    },
+    {
+      header: "Roles",
+      cell: ({ row }) => {
+        const user = row.original
+
+        if (!user.roles.length) return "-"
+
+        const roles = trimAdmin(user.roles)
+
+        return (
+          <div className="flex gap-2">
+            {roles.map((ur) => (
+              <Badge
+                variant={
+                  isAdmin(ur)
+                    ? "default"
+                    : ur === Role.Checker
+                    ? "secondary"
+                    : "outline"
+                }
+                key={ur}
+              >
+                {ur}
+              </Badge>
+            ))}
+          </div>
+        )
+      },
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const user = row.original
+
+        return (
+          <div className="flex gap-2 justify-end">
+            <Button
+              variant="outline"
+              onClick={() => {
+                setUserId(user.id)
+                setOpen(true)
+              }}
+            >
+              Remove
+            </Button>
+          </div>
+        )
+      },
+    },
+  ]
+
+  return (
+    <>
+      <RemoveUserDialog
+        id={id}
+        userId={userId}
+        open={open}
+        onClose={() => setOpen(false)}
+      />
+      <DataTable columns={columnDef} data={data} />
+    </>
+  )
 }
