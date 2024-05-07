9dac83f0245af6eac8b51589c2303b18e782993a
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2024-05-07 14:41:48 +0800

    Created error handler page

---
+++ src/app/error.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { TypographyMuted } from "@/components/ui/typography"
+import PageHeaderWithBack from "./(user)/_components/PageHeaderWithBack"
+
+export default function RootError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <div className="p-10">
+      <PageHeaderWithBack
+        title="Something went wrong!"
+        description="Kindly contact your admin and send the error
+        log below"
+      />
+      <TypographyMuted>
+        {error.stack ??
+          error.message ??
+          error.digest ??
+          error.name ??
+          "Error message not found."}
+      </TypographyMuted>
+    </div>
+  )
+}
