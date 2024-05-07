7dd88531495a4376e303a6f5f3fa09a3df7b6bca
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2024-05-07 17:05:16 +0800

    First attempt to fix IOS issue

--- src/helpers/dateHelper.ts
+++ src/helpers/dateHelper.ts
@@ -2,7 +2,7 @@ import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
 
 export const formatDateToYYYYMMDD = (dateString: string, separator: string = '-', isDateUtc = true) => {
-  const date = new Date(`${dateString}${isDateUtc ? 'z' : ''}`)
+  const date = new Date(`${dateString}${isDateUtc ? 'Z' : ''}`)
   const year = date.getFullYear()
   const month = String(date.getMonth() + 1).padStart(2, '0') // Months are zero-based
   const day = String(date.getDate()).padStart(2, '0')
@@ -11,7 +11,7 @@ export const formatDateToYYYYMMDD = (dateString: string, separator: string = '-'
 }
 
 export const formatDateToMMMDDYYYY = (dateString: string, isDateUtc = true) => {
-  const date = new Date(`${dateString}${isDateUtc ? 'z' : ''}`)
+  const date = new Date(`${dateString}${isDateUtc ? 'Z' : ''}`)
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const month = months[date.getMonth()];
   const day = date.getDate();
@@ -22,7 +22,7 @@ export const formatDateToMMMDDYYYY = (dateString: string, isDateUtc = true) => {
 
 export const timeAgo = (dateString: string) => {
   TimeAgo.addLocale(en)
-  const date = new Date(`${dateString}z`)
+  const date = new Date(`${dateString}Z`)
   const timeAgo = new TimeAgo('en-US')
   const result = timeAgo.format(date)
   return result
