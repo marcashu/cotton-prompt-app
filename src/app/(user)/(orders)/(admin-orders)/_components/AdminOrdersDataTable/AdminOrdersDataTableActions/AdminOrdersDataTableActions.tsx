a3b6fde513cc99b04f08d2b7761d55316514740d
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2024-06-12 15:11:26 +0800

    Enabled resend for customer review for orders sent for printing

--- src/app/(user)/(orders)/(admin-orders)/_components/AdminOrdersDataTable/AdminOrdersDataTableActions/AdminOrdersDataTableActions.tsx
+++ src/app/(user)/(orders)/(admin-orders)/_components/AdminOrdersDataTable/AdminOrdersDataTableActions/AdminOrdersDataTableActions.tsx
@@ -79,8 +79,7 @@ export default function AdminOrdersDataTableActions({
           />
         )}
         {(order.customerStatus === CustomerStatus.ForReview ||
-          (order.customerStatus === CustomerStatus.Accepted &&
-            adminStatus === AdminStatus.Completed) ||
+          order.customerStatus === CustomerStatus.Accepted ||
           (order.customerStatus === CustomerStatus.ChangeRequested &&
             !order.changeRequestOrderId)) && (
           <ResendForCustomerReviewAction id={order.id} onResend={onResend} />
