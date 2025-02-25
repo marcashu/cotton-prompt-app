39e0dcab90e067dc862d0468210214c59c367d09
horvathzsombor2005@gmail.com <horvathzsombor2005@gmail.com>
2025-02-25 16:58:46 +0100

    Fix

--- src/components/modules/orders/view/OrderViewDesign/OrderViewDesignComment.tsx
+++ src/components/modules/orders/view/OrderViewDesign/OrderViewDesignComment.tsx
@@ -41,7 +41,7 @@ export default function OrderViewDesignComment({
     postComment(
       id,
       comment,
-      isAdmin ? "00000000-0000-0000-0000-000000000000" : session.userId,
+      isAdmin ? "99999999-9999-9999-9999-999999999999" : session.userId,
       orderId
     )
       .then(() => {
